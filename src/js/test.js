e03836b2b0b29d5c48701ba66957185be7b9a972
AndreasDEV <andreasmrdja16@gmail.com>
2018-12-08 18:08:14 +0100

    Added Babel. More WebPack functionality. + NPM Packages | Update #3 |

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,2 @@
 console.log('Imported Modules');
+export default 16;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');   // path package, stored in path const var.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js', // Entry point
+    entry: ['@babel/polyfill', './src/js/index.js'], // Entry point
     output: {
-        path: path.resolve(__dirname, 'dist/js'),   // Outputs bundle to this directory
-        filename: 'bundle.js'   // with this filename
+        path: path.resolve(__dirname, 'dist'),   // Outputs bundle to this directory
+        filename: 'js/bundle.js'   // with this filename
     },
+    devServer: {
+        contentBase: './dist'   // live server direction
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,  // checks for all possible files that has .js in their name
+                exclude: /node_modules/,    // excluding node_modules folder
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };

49a25c801a1a42d1b4c2d994deea209ae34069ed
AndreasDEV <andreasmrdja16@gmail.com>
2018-12-08 19:08:32 +0100

    Finished building Search model and controller. RFI. | Update #4 |

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,45 @@
-// Global app controller
-import num from './test';
-console.log(`Importing ${num}`);
+/*
+import str from './modules/Search'; // FIRST WAY OF IMPORTING
+import { add as a, multiply, ID } from './views/searchView'; // SECOND WAY OF IMPORTING
+import * as searchView from  './views/searchView'   // THIRD WAY OF IMPORTING | Importing everything from directory
+
+//console.log(`Using imported functions! ${a(ID, 2)} and ${multiply(3, 5)}. ${str}.`);  // SECOND WAY OF USING IMPORTS
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}.`);    // THIRD WAY OF USING IMPORTS
+*/
+
+
+// --- IMPORTING MODELS --- \\
+import Search from './models/Search';
+
+// Global State of the app 
+// - search object
+// - current recipe object
+// - shopping list object
+// - liked recipes
+const state = {};
+
+// FUNCTION
+const controlSearch = async () => {
+    // Get a query from the view
+    const query = 'pizza' // TODO
+
+    if (query) {
+        // New Search object and add to the state
+        state.search = new Search(query);
+
+        // Prepare UI for results
+
+        // Search for recipes
+        await state.search.getResults(); 
+
+        // Render results on UI
+        console.log(state.search.recipes);
+    }
+}
+
+
+// -- EVENT LISTENER -- | Search Form
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault(); // Prevents page from reloading each time when we click element(e)
+    controlSearch();
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+// Importing Packages
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    // Making AJAX call | - Getting recipes from food2fork | SEARCH
+    async getResults() {    // Query will always be in the object once we call getResults method.
+        const apikey = '1a3554833bfedb915df1c4a71f61bb53';
+        const res = await axios(`https://www.food2fork.com/api/search?key=${apikey}&q=${this.query}`);   // res stands for result
+        try {
+            this.recipes = res.data.recipes;
+            //console.log(this.recipes);   // Displays recipes only (except whole obj)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}

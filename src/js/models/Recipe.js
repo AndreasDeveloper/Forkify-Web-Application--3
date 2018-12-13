926260b3a18c92f18985a23178828ff077a47d21
AndreasDEV <andreasmrdja16@gmail.com>
2018-12-13 20:24:31 +0100

    Finished building "Recipe" model. Second part. RFI. | Update #10 |

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -28,4 +28,60 @@ export default class Recipe {
     calcServings() {
         this.servings = 4;
     }
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds']; // Long name
+        const unitShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound']; // Short name
+
+        const newIngredients = this.ingredients.map(el => { // el is EACH element (string) in ingredients data that api provided, mapped trough
+            // Uniform units (units should be the same)
+            let ingredient = el.toLowerCase(); // Put words to lowercase
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitShort[i]); // Replace unitLong el with unitShort el. Using unit as UNIT (tablespoon etc.) and i as index (position in arr). replace method for replacing unit parameter with unitShort[i]
+            });
+            // Remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' '); // Gets rid of parentheses
+
+            // Parse ingredients into count, unit and ingredient itself
+            const arrIng = ingredient.split(' '); // if there is a space between the words, each word will become a new element
+            const unitIndex = arrIng.findIndex(el2 => unitShort.includes(el2));
+
+            let objIng;
+            if (unitIndex > -1) {
+                // There is a unit
+                // Ex. 4 1/2 cups, arrCount is [4, 1/2] --> eval("4+1/2") eval() function will evaluate this string as JS number and do the math | result is 4.5
+                // Ex. 4 cups, arrCount is [4]
+                const arrCount = arrIng.slice(0, unitIndex);  // Start from 0 and go until unitIndex string
+                let count;
+                if (arrCount.length === 1) { // length defines how many els are there before unitIndex
+                    count = eval(arrIng[0].replace('-', '+')); // Replace - with + and evalute numbers and do math
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+')); // Start slice from pos 0 until unitIndex, join those strings and evaluate them
+                }
+
+                objIng = {
+                    count: count, // Can be just count
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ') // Slice starts right after unitIndex and goes to the end, and then joins the string
+                };
+
+            } else if (parseInt(arrIng[0], 10)) { // Converts first element in string (if its a string number) and parse/convert it to integer (number with base of 10)
+                // There is no unit, but the 1st element is a number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ') // Start from position 1 (array based) and go until the end of the string. Excluding first position which is parse(argIng[0], 10) case. Put numbers back to strings using Join method
+                }
+            } else if (unitIndex === -1) {
+                // There is no unit and no number in 1st position (1st else if case)
+                objIng = {
+                    count: 1, // If its empty, shows 1
+                    unit: '',
+                    ingredient: ingredient // or just ingredient in obj
+                }
+            }
+            
+            return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
 };
